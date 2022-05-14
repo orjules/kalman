53d704f41b97d38de54c83b4d39896ad6add58f8
Julius Gilka-Bötzow <j.gilkaboetzow@gmail.com>
2022-05-14 13:03:07 +0200

    Refine R Script

--- scalar_filter/plotting/scalarKalman.r
+++ scalar_filter/plotting/scalarKalman.r
@@ -1,16 +1,27 @@
-data <- read.table("~/Documents/STAR/Kalman Filter/kalman/scalar_filter/plotting/rust_out.txt", header = TRUE, sep = ",", dec = ".")
+data <- read.table(
+    "~/Documents/STAR/Kalman Filter/kalman/scalar_filter/plotting/rust_out.txt",
+    header = TRUE,
+    sep = ",",
+    dec = ".")
 
 plot(
     data$Iteration, data$Estimate,
     col = "red",
+    type = "l",
     ylim = range(15:31),
-    xlab = "Iteration", ylab = "Estimated Temperature",
+    xlab = "Iteration",
+    ylab = "Temperature",
     main = "Kalman Filter simulation",
-    type = "l"
+    sub = sprintf("Initial uncertainty: %2.0f, Measurement Uncertainty: %1.1f",
+            data[1, "Est_Uncert"],
+            data[1, "Meas_Uncert"])
 )
-points(data$Iteration, data$Measurement)
-abline(h=data$Real)
-legend("topleft", 
-       legend = c("Estimate", "Measurement"), 
-       col = c("red", "black"),
-       pch=c(1,1))
+points(data$Iteration, data$Measurement, col = "blue")
+abline(h = data$Real)
+legend("top",
+        inset = 0.01,
+        bty = "n",
+        y.intersp = 1.3,
+        legend = c("Estimate", "Measurement", "Real Temperature"),
+        col = c("red", "blue", "black"),
+        pch = c(1, 1, 1))
